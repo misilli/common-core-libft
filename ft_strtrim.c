6f643f35210203ec28e3fb5f16f258874cf402b4
misilli <77497957+misilli@users.noreply.github.com>
2026-08-08 16:24:59 +0300

    Implement initial version of ft_strtrim function with basic structure

---
+++ ft_strtrim.c
@@ -0,0 +1,19 @@
+
+#include "libft.h"
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+    char *final;
+    size_t i;
+
+    i = 0;
+    while (s1[i] != '\0')
+    {
+        while (st)
+        {
+            /* code */
+        }
+        i++;
+    }
+    
+}
