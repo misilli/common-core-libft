910bfcc6d2be911d8799c105dea1040950ce9ce8
misilli <77497957+misilli@users.noreply.github.com>
2026-08-10 18:41:16 +0300

    Implement ft_split function to split a string into an array of substrings based on a delimiter

---
+++ ft_split.c
@@ -0,0 +1,37 @@
+char	**ft_split(char const *s, char c)
+{
+	char **final;
+	int i;
+	int word;
+	int length;
+	int j;
+
+	j = 0;
+
+	i = 0;
+	word = 0;
+	while (s[i])
+    {
+        if (s[i] != c && (i == 0 || s[i - 1] == c))
+            word++;
+        i++;
+    }
+	final = malloc(sizeof(char *) * (word + 1));
+	if (!final)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		length = 0;
+		while (s[i] && s[i] != c)
+		{
+			length++;
+			i++;
+		}
+		i++;
+		final[j] = malloc(sizeof(char) * (length + 1));
+		if (!final[j])
+			return (NULL);
+		j++;
+	}
+}
