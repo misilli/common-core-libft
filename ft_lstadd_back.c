8c4f5832e298cbc72dbb61677b079c3208cd55a3
misilli <77497957+misilli@users.noreply.github.com>
2026-08-14 03:24:30 +0300

    Fix ft_lstadd_back function and implement ft_lstsize function

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -3,30 +3,11 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	while (*lst)
-	{
-		lst = ;
-	}
-	lst->next = new;
-
-	
-
-}
-/*
-#include "libft.h"
-
-void ft_lstadd_back(t_list **lst, t_list *new)
-{
-    t_list *last;
-
-    if (*lst == NULL)
-    {
-        *lst = new;
+    if(!lst)
         return;
-    }
-    last = *lst;
-    while (last->next != NULL)
-        last = last->next;
-    last->next = new;
+	while ((*lst)->next != NULL)
+	{
+		lst = &(*lst)->next;
+	}
+	(*lst)->next = new;
 }
-*/
---
+++ ft_lstsize.c
@@ -0,0 +1,15 @@
+
+#include "libft.h"
+
+unsigned int ft_lstsize(t_list *lst)
+{
+    int     i;
+    i = 0;
+    while (lst)
+    {
+        lst = lst->next;
+        i++;
+    }
+    
+    return (i);
+}
